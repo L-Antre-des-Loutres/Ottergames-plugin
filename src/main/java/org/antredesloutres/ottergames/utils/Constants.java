773760b3a752a94cccd51d6b0c4ffaef0dbcde55
Corentin COTTEREAU <109530954+Corentin-cott@users.noreply.github.com>
2026-05-02 02:00:25 +0200

    Add utility classes for structure management and update plugin commands

---
+++ src/main/java/org/antredesloutres/ottergames/utils/Constants.java
@@ -0,0 +1,9 @@
+package org.antredesloutres.ottergames.utils;
+
+public final class Constants {
+
+    private Constants() {} // This class is not meant to be instantiated
+
+    public static final String PLUGIN_NAME = "Ottergames";
+
+}
